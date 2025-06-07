3885390f0cd0227b6eec54aafdb6405445124fb9
Hasegawa Raku <raku@exhibition.jp>
2025-06-07 15:46:13 +0900

    Move shader codes to another file

--- crates/zintl-render/src/wgpu.rs
+++ crates/zintl-render/src/wgpu.rs
@@ -18,50 +18,7 @@ pub struct Texture {
     height: u32,
 }
 
-const SRC: &str = r###"
-
-struct Uniforms {
-    ortho : mat4x4<f32>,
-};
-
-@group(0) @binding(0)
-var<uniform> uniforms : Uniforms;
-
-struct VertexInput {
-    @location(0) position: vec2<f32>,
-    @location(1) tex_coords: vec2<f32>,
-}
-
-struct VertexOutput {
-    @builtin(position) clip_position: vec4<f32>,
-    @location(0) tex_coords: vec2<f32>,
-}
-
-@vertex
-fn vs_main(
-    model: VertexInput,
-) -> VertexOutput {
-    var out: VertexOutput;
-    out.tex_coords = model.tex_coords;
-    out.clip_position = uniforms.ortho * vec4<f32>(model.position, 0.0, 1.0);
-    return out;
-}
-
-/*@vertex
-fn vs_main(@location(0) position: vec2<f32>) -> @builtin(position) vec4<f32> {
-    return vec4<f32>(position, 0.0, 1.0);
-}*/
-
-@group(1) @binding(0)
-var t_diffuse: texture_2d<f32>;
-@group(1) @binding(1)
-var s_diffuse: sampler;
-
-@fragment
-fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
-}
-"###;
+const FILL_RECT_SHADER_SRC: &str = include_str!("../shaders/fill_rect.wgsl");
 
 pub type TextureId = usize;
 
@@ -273,7 +230,7 @@ impl<'a> WgpuApplication<'a> {
 
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: None,
-            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(SRC)),
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(FILL_RECT_SHADER_SRC)),
         });
 
         let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
