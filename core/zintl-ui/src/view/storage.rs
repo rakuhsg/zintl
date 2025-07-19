36c1d7fc74674fd9efa4457155d49375263d0582
Hasegawa, Raku <raku@exhibition.jp>
2025-07-19 13:27:48 +0900

    Add insert method

--- core/zintl-ui/src/view/storage.rs
+++ core/zintl-ui/src/view/storage.rs
@@ -1,17 +1,21 @@
 use std::any::Any;
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 /// Persistent key-value storage for [`View`].
 #[derive(Clone, Debug)]
 pub struct Storage {
-    data: Arc<HashMap<String, Arc<dyn Any>>>,
+    data: Arc<Mutex<HashMap<String, Arc<dyn Any>>>>,
 }
 
 impl Storage {
     pub fn new() -> Self {
         Storage {
-            data: HashMap::new().into(),
+            data: Mutex::new(HashMap::new()).into(),
         }
+    }
+
+    pub fn insert<T: 'static>(&mut self, key: String, data: T) {
+        self.data.lock().unwrap().insert(key, Arc::new(data)).unwrap();
     }
 }
