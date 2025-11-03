0bf02f3432ef1c1b4b58606e721e3a5c88a9e2e4
Hasegawa, Raku <raku@exhibition.jp>
2025-11-03 20:34:27 +0900

    test: Update tests

--- test/zintl-test/src/tests.rs
+++ test/zintl-test/src/tests.rs
@@ -4,38 +4,42 @@ mod test {
     use zintl_state::*;
 
     use crate::{runner::*, views::*};
+    /*
+        #[test]
+        fn custom_hello_world() {
+            let app = App::new(TestLabel::new("hello, world!".to_string()));
+            let mut runner = Runner::new(app);
+            assert_eq!(runner.render(TestEvent::RedrawRequested), "hello, world!");
+        }
 
-    #[test]
-    fn custom_hello_world() {
-        let app = App::new(TestLabel::new("hello, world!".to_string()));
-        let mut runner = Runner::new(app);
-        assert_eq!(runner.render(TestEvent::RedrawRequested), "hello, world!");
-    }
-
-    #[test]
-    fn stack() {
-        let app =
-            App::new(TestStack::new().children(v![TestLabel::new("hello, world!".to_string())]));
-        let mut runner = Runner::new(app);
-        assert_eq!(runner.render(TestEvent::RedrawRequested), "hello, world!");
-    }
-
+        #[test]
+        fn stack() {
+            let app =
+                App::new(TestStack::new().children(v![TestLabel::new("hello, world!".to_string())]));
+            let mut runner = Runner::new(app);
+            assert_eq!(runner.render(TestEvent::RedrawRequested), "hello, world!");
+        }
+    */
     #[test]
     fn stateful() {
-        let app = App::new(StatefulView::new(marked!(), "Hi".to_string(), |value| {
-            let mut v = value.clone();
-            v![
-                TestStack::new().children(v![TestButton::new(value.value().to_string()).on_click(
-                    move || {
-                        println!("Click event triggered.");
-                        v.set("Clicked".to_string().to_owned());
-                    }
-                ),])
-            ]
-        }));
+        let app = App::new(TestStack::new().children(v![StatefulView::new(
+            marked!(),
+            "Hi".to_string(),
+            |value| {
+                let mut v = value.clone();
+                v![
+                    TestStack::new().children(v![TestButton::new(value.value().to_string())
+                        .on_click(move || {
+                            println!("Click event triggered.");
+                            v.set("Clicked".to_string().to_owned());
+                        }),])
+                ]
+            }
+        )]));
         let mut runner = Runner::new(app);
         assert_eq!(runner.render(TestEvent::RedrawRequested), "Hi");
         assert_eq!(runner.render(TestEvent::Click), "Hi");
+        assert_eq!(runner.render(TestEvent::RedrawRequested), "Clicked");
         assert_eq!(runner.render(TestEvent::RedrawRequested), "Clicked");
     }
 }
--- web/zintl-wasm/src/lib.rs
+++ web/zintl-wasm/src/lib.rs
@@ -1,3 +1,7 @@
+mod runtime;
+mod views;
+
+pub use views::*;
 use zintl::App;
 
 pub fn run_app(app: App) {
