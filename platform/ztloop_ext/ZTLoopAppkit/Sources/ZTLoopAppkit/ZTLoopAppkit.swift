a6a93f607c9f058a1832e18d6b97121c6f8d3fe1
Hasegawa, Raku <raku@exhibition.jp>
2025-12-20 22:02:23 +0900

    ztloop: Add Appkit event loop implementation

--- platform/ztloop_ext/ZTLoopAppkit/Sources/ZTLoopAppkit/ZTLoopAppkit.swift
+++ platform/ztloop_ext/ZTLoopAppkit/Sources/ZTLoopAppkit/ZTLoopAppkit.swift
@@ -1,7 +1,39 @@
 import AppKit
+import CoreFoundation
 
 @MainActor
 class ZTLoopState {
     static let shared = ZTLoopState()
+
+    var loop: CFRunLoop?;
 }
 
+@_cdecl("ztloop_init")
+func ztloopInit() {
+    let loop = RunLoop.current.getCFRunLoop();
+    Task {@MainActor in
+        assert(ZTLoopState.shared.loop == nil, "ZTLoopState is already initialized")
+        
+        let app = NSApplication.shared
+        app.setActivationPolicy(.regular)
+        
+        ZTLoopState.shared.loop = loop
+    }
+}
+
+@_cdecl("ztloop_run")
+func ztloopRun() {
+    Task { @MainActor in
+        guard let loop = ZTLoopState.shared.loop else {
+            assertionFailure("ZTLoopState.loop is not initialized")
+            return
+        }
+
+        CFRunLoopPerformBlock(loop, CFRunLoopMode.commonModes.rawValue) {
+            //
+        }
+        CFRunLoopWakeUp(loop)
+    }
+    
+    RunLoop.current.run()
+}
