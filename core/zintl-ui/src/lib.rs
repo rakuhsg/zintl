6d00a7b4811bc9dbf20ee2fccc57e47e4c4360f7
Hasegawa, Raku <raku@exhibition.jp>
2026-01-03 19:54:06 +0900

    misc: Remove zintl mvq

--- core/zintl-ui/src/lib.rs
+++ core/zintl-ui/src/lib.rs
@@ -1,17 +1,9 @@
 mod app;
 mod event;
-#[cfg(feature = "mvq")]
-mod model;
-#[cfg(feature = "mvq")]
-mod query;
 mod render;
 mod view;
 
 pub use app::*;
 pub use event::*;
-#[cfg(feature = "mvq")]
-pub use model::*;
-#[cfg(feature = "mvq")]
-pub use query::*;
 pub use render::*;
 pub use view::*;
